01878ecfec650692234a82914458d570bf26cadc
Tom <tmrdrgz@ymail.com>
2020-07-30 11:12:11 +0100

    added radio buttons

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,7 +7,9 @@ function App() {
 
   let [quizData, loadQuizData] = useState([]);
 
-  console.log(`This is the initial quizData: ${quizData}`);
+  let [userAnswer, setUserAnswer] = useState(null);
+
+  // console.log(`This is the initial quizData: ${quizData}`);
 
   useEffect( () => {
     getApi();
@@ -15,24 +17,78 @@ function App() {
   }, [])
   
   const createQuestionCard = (quizDatum, index) => {
-    console.log("Extracting questions");
-    console.log(quizDatum.question);
+    // console.log("Extracting questions");
+    // console.log(quizDatum.question);
+
+    let correctAnswer = [quizDatum.correct_answer];
+    // console.log(`The correct answer is ${correctAnswer}`);
+    let incorrectAnswers = quizDatum.incorrect_answers;
+    // console.log(`The incorrect answers are ${incorrectAnswers}`);
+    let allAnswers = correctAnswer.concat(incorrectAnswers);
+    // console.log(allAnswers);
+
+    // Shuffle function
+
+    let randomiseAnswers = (array) => {
+      for (let i = 0; i < 10; i++) {
+          for (let i = array.length - 1; i > 0; i--) {
+              let newIndex = Math.floor(Math.random() * (i + 1));
+              let temp = array[i];
+              array[i] = array[newIndex];
+              array[newIndex] = temp;
+          }
+          return array
+      };
+    };
+
+    let randomisedAnswers = randomiseAnswers(allAnswers);
+
+    // console.log(randomisedAnswers);
+
+    // Submit function
+
+
+    // const submitAnswer = (event) => {
+    //   setUserAnswer(event.target.value), () => {
+    //     console.log(userAnswer);
+    //   }
+
+      
+    // }
+
+    // const submitAnswer = (event) => {
+    //   console.log(`ANSWER SUBMITTED: ${event.target.value}`);
+    //   setUserAnswer(event.target.value);
+    //   // console.log(userAnswer);
+    // }
+
+    const onValueChange = (event) => {
+      setUserAnswer(event.target.value);
+    }
+
+    const formSubmit = (event) => {
+      event.preventDefault();
+      console.log(userAnswer);
+    }
 
     return (
       <QCard 
         key={index} 
         qNo={index+1} 
         text={quizDatum.question}
-        answer1="Answer 1"
-        answer2="Answer 2"
-        answer3="Answer 3" 
+        answer1={randomisedAnswers[0]}
+        answer2={randomisedAnswers[1]}
+        answer3={randomisedAnswers[2]}
+        answer4={randomisedAnswers[3]}
+        formSubmitFunc={formSubmit} 
+        onValueChangeFunc={onValueChange}
       />
     )
   }
 
   const getApi = async () => {
 
-    const res = await axios.get('https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=multiple')
+    const res = await axios.get('https://opentdb.com/api.php?amount=10&category=27&difficulty=easy&type=multiple')
     console.log(res.data);
 
     loadQuizData(res.data.results);
--- client/src/components/qCard.js
+++ client/src/components/qCard.js
@@ -5,9 +5,48 @@ export default function QCard(props) {
     return (
         <div>
             <h2>{props.qNo} {props.text}</h2>
-            <button>{props.answer1}</button>
-            <button>{props.answer2}</button>
-            <button>{props.answer3}</button>
+            <form onSubmit={props.formSubmitFunc}>
+
+                <input 
+                    type="radio" 
+                    id="answer1" 
+                    name="questionAnswer" 
+                    value={props.answer1}
+                    onChange={props.onValueChangeFunc}
+                />
+                <label htmlFor="answer1" >{props.answer1}</label>
+                <br />
+
+                <input 
+                    type="radio" 
+                    id="answer2" 
+                    name="questionAnswer" 
+                    value={props.answer2} 
+                />
+                <label htmlFor="answer2" >{props.answer2}</label>
+                <br />
+
+                <input 
+                    type="radio" 
+                    id="answer3" 
+                    name="questionAnswer" 
+                    value={props.answer3} 
+                />
+                <label htmlFor="answer3" >{props.answer3}</label>
+                <br />
+
+                <input 
+                    type="radio" 
+                    id="answer4" 
+                    name="questionAnswer" 
+                    value={props.answer4} 
+                />
+                <label htmlFor="answer4" >{props.answer4}</label>
+                <br />
+
+                <button type="submit">Submit</button>
+
+            </form>
         </div>
     )
 }
