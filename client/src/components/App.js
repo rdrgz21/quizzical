b9dfa14569ec12c05ae99edabd1b778452e58a64
emurphynanton@gmail.com <ethan@Ethans-MacBook-Pro.local>
2020-08-02 14:01:59 +0100

    saved new update on sub master

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,70 +3,12 @@ import './App.css';
 import axios from 'axios';
 import QCard from './QCard';
 
-<<<<<<< HEAD
-function App() {
-
-  console.log("Refreshing comp");
-
-  let [quizData, loadQuizData] = useState([]);
-  let [randomAnswers, setRandomAnswers] = useState([]);
-  let [userAnswer, setUserAnswer] = useState(null);
-
-  // console.log(`This is the initial quizData: ${quizData}`);
-
-  useEffect( () => {
-    getApi();
-    savingAnswers();
-  }, [])
-
-  const getApi = async () => {
-
-    const res = await axios.get('https://opentdb.com/api.php?amount=10&category=27&difficulty=easy&type=multiple')
-    //console.log(res.data);
-
-    loadQuizData(res.data.results);
-  };
-  console.log(quizData);
-
-
-
-    function savingAnswers(){
-    
-    const randomiseAnswers = quizData && quizData.map( (quizDatum, index) => {
-      // console.log("Extracting questions");
-      // console.log(quizDatum.question);
-
-      let correctAnswer = [quizDatum.correct_answer];
-      // console.log(`The correct answer is ${correctAnswer}`);
-      let incorrectAnswers = quizDatum.incorrect_answers;
-      // console.log(`The incorrect answers are ${incorrectAnswers}`);
-      let allAnswers = correctAnswer.concat(incorrectAnswers);
-      // console.log(allAnswers);
-
-
-      // Array Shuffle function
-        for (let i = 0; i < 10; i++) {
-            for (let i = allAnswers.length - 1; i > 0; i--) {
-                let newIndex = Math.floor(Math.random() * (i + 1));
-                let temp = allAnswers[i];
-                allAnswers[i] = allAnswers[newIndex];
-                allAnswers[newIndex] = temp;
-            }
-        };
-
-
-        return allAnswers;
-    });
-
-    setRandomAnswers();
-=======
 class App extends Component {
     state = {
         quizData: [],
         randomAnswers: [],
         userAnswer: null
     }
->>>>>>> fec2358571f015b30327966a6a02c62399259b16
 
     saveAnswers = (index) => {
         if (this.state.quizData.length > 0) {
