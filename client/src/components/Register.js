446e15188038decc4ca9b442d7fb55e9e5c8623d
Tom <tmrdrgz@ymail.com>
2020-08-04 10:52:35 +0100

    added register page

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,15 +1,93 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
-export default class Register extends Component {
+class Register extends Component {
+    state = {
+        inputUsername: "",
+        inputEmailAddress: "",
+        inputPassword: "",
+        inputPassword2: "",
+        message: ""
+    }
+
+    recordUsername = (event) => {
+        this.setState({
+            inputUsername: event.target.value
+        })
+    }
+
+    recordEmail = (event) => {
+        this.setState({
+            inputEmailAddress: event.target.value
+        })
+    }
+
+    recordPassword = (event) => {
+        this.setState({
+            inputPassword: event.target.value
+        })
+    }
+
+    submitForm = (event) => {
+        event.preventDefault();
+        console.log("Submitting form");
+        this.registerUser();
+    }
+
+    registerUser = async () => {
+        console.log("Registering user");
+        
+        const userDetails = {
+            name: this.state.inputUsername,
+            email: this.state.inputEmailAddress,
+            password: this.state.inputPassword
+        }
+
+        await axios.post('/register', userDetails)
+            .then(res => {
+                console.log(res.data);
+            }).catch((error) => {
+                console.log(error)
+                console.log("There was an error");
+            });
+
+        this.setState({ 
+            inputUsername: '', 
+            inputEmailAddress: '',
+            inputPassword: '' 
+        })
+    }
+
     render() {
-        // state = {}
         return (
-            <div>
+            <React.Fragment>
                 <h1>Register Page</h1>
-                <form>
-
-                </form>
-            </div>
+                <div className="registration-form-container">
+                    <h3 className="registration-form-text heading">Register</h3>
+                    <form onSubmit={this.submitForm}>
+                        <label htmlFor="userName">Username</label>
+                        <br />
+                        <input onChange={this.recordUsername} type="text" name="userName" value={this.state.inputUsername} id="userName" required/>
+                        <br />
+                        <label htmlFor="userEmail">Email Address</label>
+                        <br />
+                        <input onChange={this.recordEmail} type="email" name="userEmail" value={this.state.inputEmailAddress} id="userEmail" required/>
+                        <br />
+                        <label htmlFor="userPassword">Password</label>
+                        <br />
+                        <input onChange={this.recordPassword} type="password" name="userPassword" value={this.state.inputPassword} id="userPassword" required/>
+                        <br />
+                        <label htmlFor="userPassword2">Re-enter Password</label>
+                        <br />
+                        <input type="password" name="userPassword2" id="userPassword2" required/>
+                        <br />
+                        <button id="register-button" type="submit">Register</button>
+                    </form>
+                    {this.state.message ? <h4 className="registration-form-text message">{this.state.message}</h4> : null}
+                </div>
+            </React.Fragment>
         )
     }
 }
+
+export default Register;
--- server.js
+++ server.js
@@ -23,6 +23,53 @@ app.get("/", (req, res) => {
     // res.render("http://localhost:3000/")
 });
 
+app.post("/register", async (req, res) => {
+    console.log("Attempting to register");
+    console.log(req.body);
+
+
+
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    // const password2 = req.body.userPassword2;
+    // const hashedPassword = await bcrypt.hash(password, 8);
+
+    // Add function to check if email already in use
+    // const otherUser = await User.find({ email: email});
+    // console.log(otherUser);
+
+    await User.create(
+        {
+            name: name,
+            email: email,
+            password: password
+        }
+    )
+    res.send("User registered");
+
+    // if (otherUser.length > 0) {
+    //     res.render("register", {
+    //         message: "Sorry, that email is already in use"
+    //     });
+    // } else if (password == password2) {
+    //     await User.create(
+    //         {
+    //             name: name,
+    //             email: email,
+    //             password: hashedPassword
+    //         }
+    //     )
+    //     res.render("register", {
+    //         message: "User successfully registered"
+    //     })
+    // } else {
+    //     res.render("register", {
+    //         message: "Please ensure both passwords entered match"
+    //     });
+    // }
+})
+
 app.listen( 5000, () => {
     console.log("Server is running on port 5000");
 });
