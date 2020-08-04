0aea208cd314438099ab42da52e8f6147780c3c6
Tom <tmrdrgz@ymail.com>
2020-08-04 11:22:08 +0100

    added register page

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const bcrypt = require('bcrypt');
 const User = require('./models/user');
 
 dotenv.config( { path: './.env' } );
@@ -32,42 +33,34 @@ app.post("/register", async (req, res) => {
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
-    // const password2 = req.body.userPassword2;
-    // const hashedPassword = await bcrypt.hash(password, 8);
+
+    const hashedPassword = await bcrypt.hash(password, 8);
 
     // Add function to check if email already in use
-    // const otherUser = await User.find({ email: email});
-    // console.log(otherUser);
+    const otherUser = await User.find({ email: email});
+    console.log(otherUser);
 
-    await User.create(
-        {
-            name: name,
-            email: email,
-            password: password
-        }
-    )
-    res.send("User registered");
+    // await User.create(
+    //     {
+    //         name: name,
+    //         email: email,
+    //         password: hashedPassword
+    //     }
+    // )
+    // res.send("User registered");
 
-    // if (otherUser.length > 0) {
-    //     res.render("register", {
-    //         message: "Sorry, that email is already in use"
-    //     });
-    // } else if (password == password2) {
-    //     await User.create(
-    //         {
-    //             name: name,
-    //             email: email,
-    //             password: hashedPassword
-    //         }
-    //     )
-    //     res.render("register", {
-    //         message: "User successfully registered"
-    //     })
-    // } else {
-    //     res.render("register", {
-    //         message: "Please ensure both passwords entered match"
-    //     });
-    // }
+    if (otherUser.length > 0) {
+        res.send("Sorry, that email is already in use");
+    } else {
+        await User.create(
+            {
+                name: name,
+                email: email,
+                password: hashedPassword
+            }
+        )
+        res.send("User successfully registered");
+    }
 })
 
 app.listen( 5000, () => {
