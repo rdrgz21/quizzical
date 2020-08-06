3936e707ad5079b0f862e1a679d0219a50568794
emurphynanton@gmail.com <ethan@Ethans-MacBook-Pro.local>
2020-08-06 16:50:46 +0100

    styled qCard/quiz css

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import './App.css';
 import axios from 'axios';
-import QCard from './QCard';
+import QCard from './qCard';
 import ResultsCard from './ResultsCard';
 import QuizSelection from './QuizSelection';
 import { Redirect } from 'react-router-dom';
@@ -278,19 +278,19 @@ class Quiz extends Component {
                 <React.Fragment>
                   
                         <div className="quiz-body">
-                            <h1>Welcome to the quiz, {this.state.userName}!!</h1>
-                            { !this.state.loggedIn && <h1>You are not logged in</h1>}
-                            { !this.state.quizSelected && this.state.loggedIn ? (
-                                <QuizSelection selectCategoryFunc={this.selectCategory} selectDifficultyFunc={this.selectDifficulty} selectQuizFunc={this.selectQuiz} />
-                            ) : null }
-                            
-                            
-                            { this.state.quizSelected ? (
-                                <div>
-                                    <p>Timer: {this.state.seconds} </p> 
+                        
+                        { this.state.quizSelected ? (
+                                <div className="timerBox">
+                                    <p id="timer">Timer: {this.state.seconds} </p> 
                                     {/* <button onClick={this.startTimer}>To start</button>
                                     <button onClick={this.stopTimer}>To stop</button> */}
                                 </div>
+                            ) : null }
+
+                            <h1 id="greeting">Welcome to the quiz, {this.state.userName}!!</h1>
+                            { !this.state.loggedIn && <h1>You are not logged in</h1>}
+                            { !this.state.quizSelected && this.state.loggedIn ? (
+                                <QuizSelection selectCategoryFunc={this.selectCategory} selectDifficultyFunc={this.selectDifficulty} selectQuizFunc={this.selectQuiz} />
                             ) : null }
     
                             { this.state.quizSelected && this.state.questionsAnswered >= 0 ? displayCards : null}
