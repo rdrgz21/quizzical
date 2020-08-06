78ba33ce02092117ed88f07487c79747857ded1a
Tom <tmrdrgz@ymail.com>
2020-08-06 14:51:00 +0100

    styled qcard

--- client/src/components/qCard.js
+++ client/src/components/qCard.js
@@ -5,49 +5,52 @@ import './QCard.css';
 export default function QCard(props) {
     return (
         <div className="qcard">
-            <h2>{props.qNo} {props.text}</h2>
+            <h2>Question {props.qNo}</h2>
+            <h2>{props.text}</h2>
             <form name={props.qNo} onSubmit={props.formSubmitFunc}>
-
-                <input 
-                    type="radio" 
-                    id="answer1" 
-                    name="questionAnswer" 
-                    value={props.answer1}
-                    onChange={props.onValueChangeFunc}
-                />
-                <label htmlFor="answer1" >{props.answer1}</label>
+                <div className="option">
+                    <input 
+                        type="radio" 
+                        id="answer1" 
+                        name="questionAnswer" 
+                        value={props.answer1}
+                        onChange={props.onValueChangeFunc}
+                    />
+                    <label htmlFor="answer1" >{props.answer1}</label>
+                </div>
+                <div className="option">
+                    <input 
+                        type="radio" 
+                        id="answer2" 
+                        name="questionAnswer" 
+                        value={props.answer2}
+                        onChange={props.onValueChangeFunc}
+                    />
+                    <label htmlFor="answer2" >{props.answer2}</label>
+                </div>
+                <div className="option">
+                    <input 
+                        type="radio" 
+                        id="answer3" 
+                        name="questionAnswer" 
+                        value={props.answer3}
+                        onChange={props.onValueChangeFunc} 
+                    />
+                    <label htmlFor="answer3" >{props.answer3}</label>
+                </div>
+                <div className="option">
+                    <input 
+                        type="radio" 
+                        id="answer4" 
+                        name="questionAnswer" 
+                        value={props.answer4}
+                        onChange={props.onValueChangeFunc} 
+                    />
+                    <label htmlFor="answer4" >{props.answer4}</label>
+                </div>
                 <br />
-
-                <input 
-                    type="radio" 
-                    id="answer2" 
-                    name="questionAnswer" 
-                    value={props.answer2}
-                    onChange={props.onValueChangeFunc}
-                />
-                <label htmlFor="answer2" >{props.answer2}</label>
                 <br />
-
-                <input 
-                    type="radio" 
-                    id="answer3" 
-                    name="questionAnswer" 
-                    value={props.answer3}
-                    onChange={props.onValueChangeFunc} 
-                />
-                <label htmlFor="answer3" >{props.answer3}</label>
-                <br />
-
-                <input 
-                    type="radio" 
-                    id="answer4" 
-                    name="questionAnswer" 
-                    value={props.answer4}
-                    onChange={props.onValueChangeFunc} 
-                />
-                <label htmlFor="answer4" >{props.answer4}</label>
-                <br />
-                <button type="submit">{props.submitBtnText}</button>
+                <button className="qcard-button" type="submit">{props.submitBtnText}</button>
             </form>
         </div>
     )
